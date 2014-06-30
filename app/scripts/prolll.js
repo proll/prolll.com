e857d6f18e5dc15d5818f202f73505cf57030f89
proll <gleb.proll@gmail.com>
2014-06-30 12:17:48 +0400

    email test

--- app/scripts/prolll.js
+++ app/scripts/prolll.js
@@ -22,16 +22,11 @@ jQuery(function() {
 	jQuery.fn.n33_formerize=function(){var _fakes=new Array(),_form = jQuery(this);_form.find('input[type=text],textarea').each(function() { var e = jQuery(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = jQuery(this); var x = jQuery(jQuery('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = jQuery(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = jQuery(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { jQuery(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); jQuery(this).find('select').val(jQuery('option:first').val()); jQuery(this).find('input,textarea').each(function() { var e = jQuery(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
 	jQuery.browser={};(function(){jQuery.browser.msie=false;jQuery.browser.version=0;if(navigator.userAgent.match(/MSIE ([0-9]+)\./)){jQuery.browser.msie=true;jQuery.browser.version=RegExp.$1;}})();
 
-	var	_bh = jQuery('body, html'),
+	var	_bh = jQuery('body, html')
+		_b = jQuery('body'),
 		_window = jQuery(window),
 		_nav = jQuery('#nav');
 
-	// Forms
-	if (jQuery.browser.msie && jQuery.browser.version <= 9)
-		jQuery('form').n33_formerize();
-
-	jQuery('form .form-button-submit').click(function(e) { e.preventDefault(); jQuery(this).closest('form').submit(); });
-	jQuery('form .form-button-reset').click(function(e) { e.preventDefault(); jQuery(this).closest('form')[0].reset(); });
 
 	// Links
 	jQuery('a').click(function(e) {
@@ -46,22 +41,46 @@ jQuery(function() {
 	});
 
 
+	// Forms
+
+	var that = this,
+		loading = false,
+		$mail_form = $('form.mail-form'),
+		$name = 	$mail_form.find('[name=name]'),
+		$email = 	$mail_form.find('[name=email]'),
+		$subject = 	$mail_form.find('[name=subject]'),
+		$message = 	$mail_form.find('[name=message]'),
+		$error = 	$mail_form.find('.mail-form__error');
+
 	 function isEmail(email) {
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return re.test(email)
-	};
+	}
 
+	function showError(txt, name) {
+		$error.html(txt);
+		_b.toggleClass('error', true);
+		if(!!name) {
+			$mail_form.find('[name='+name+']').focus();
+		}
+	}
 
-	var that = this,
-		$mail_form = $('form.mail-form'),
-		$name = $mail_form.find('[name=name]'),
-		$email = $mail_form.find('[name=email]'),
-		$subject = $mail_form.find('[name=subject]'),
-		$message = $mail_form.find('[name=message]');
+	if (jQuery.browser.msie && jQuery.browser.version <= 9)
+		jQuery('form').n33_formerize();
+
+	$('form .form-button-submit').click(function(e) { e.preventDefault(); jQuery(this).closest('form').submit(); hideError();});
+	$('form .form-button-reset').click(function(e) { e.preventDefault(); jQuery(this).closest('form')[0].reset(); $name.focus(); hideError()});
+
+	function hideError() {
+		_b.toggleClass('error', false);
+	}
+
+	$mail_form.find(':input').on('keypress', hideError);
 
 	$mail_form.on('submit', function(e) {
 		if(e && e.preventDefault) {
 			e.preventDefault();
+			e.stopPropagation();
 		}
 
 		// validate
@@ -79,13 +98,49 @@ jQuery(function() {
 		} else if(!message.length) {
 			showError('Write me something, it would be great', 'message');
 		} else {
-			$.ajax();
+			if(loading) {
+				return false;
+			}
+
+			loading = true;
+
+			$.ajax({
+				type: 		'post',
+				url: 		'/ml.php',
+				data: {
+					name : name,
+					email : email,
+					subject : subject,
+					message : message
+				}
+			})
+			.success(function(response, status, xhr){
+				success(response, status, xhr);
+			})
+			.error(function(data){
+				error(data);
+			});
 		}
+	});
 
 
-		return false;
-	})
 
+	function success(response, status, xhr){
+		loading = false;
+		console.log(response);
+		// if (!resp || !resp.success) {
+		// 	this.error();
+		// } else {
+		// }
+	}
 
+	function error(xhr, status, desc) {
+		loading = false;
+		console.log(xhr, status, desc);
+		if(!!xhr.responseText) {
+			showError(xhr.responseText);
+		}
+		return true;
+	}
 
 });
